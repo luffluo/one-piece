31aab3b2d3065ce47a305281fb537b397629269b
qiyueshiyi <qiyueshiyi@outlook.com>
2017-02-13 21:31:56 +0800

    Add relationships table

---
+++ database/migrations/2017_02_11_000000_create_relationships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelationshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relationships', function (Blueprint $table) {
+            $table->integer('content_id')->unsigned();
+            $table->integer('meta_id')->unsigned();
+            $table->primary([
+                'content_id',
+                'meta_id'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relationships');
+    }
+}
