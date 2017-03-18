801bb72119725c178b1e8421e39be5e89effed0f
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-18 22:12:12 +0800

    Add site description option

--- app/Http/Controllers/Admin/OptionController.php
+++ app/Http/Controllers/Admin/OptionController.php
@@ -8,14 +8,16 @@ class OptionController extends Controller
 {
     public function index()
     {
-        $site_name = option('site.name');
+        $site_name        = option('site.name');
+        $site_description = option('site.description');
 
-        return admin_view('option.index', compact('site_name'));
+        return admin_view('option.index', compact('site_name', 'site_description'));
     }
 
     public function store()
     {
         option(['site.name', request()->input('site_name')]);
+        option(['site.description', request()->input('site_description')]);
 
         return redirect()->route('admin.options.index');
     }
--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -1,12 +1,12 @@
 @extends('admin::layouts.layout')
-@section('title')区域列表 - 区域管理@endsection
+@section('title')网站配置 - 系统设置@endsection
 
 @section('content')
     <div class="page clearfix">
         <ol class="breadcrumb breadcrumb-small">
             <li>后台首页</li>
-            <li>系统管理</li>
-            <li>全局管理</li>
+            <li>系统设置</li>
+            <li>网站配置</li>
         </ol>
         <div class="page-wrap">
             <div class="row">
@@ -14,7 +14,7 @@
                 <div class="col-md-12">
                     <div class="panel panel-lined clearfix mb30">
                         <div class="panel-heading mb20">
-                            <i>编辑提现</i>
+                            <i>网站配置</i>
                         </div>
                         <div>
 
@@ -27,10 +27,19 @@
                                     <div class="col-md-4">
                                         <input class="form-control" type="text" name="site_name" id="site_name"
                                                value="{{ old('site_name', $site_name) }}"
-                                               placeholder="请输入网站名称"
-                                        >
+                                               placeholder="请输入网站名称">
                                     </div>
                                 </div>
+
+                                <div class="form-group form-group-sm">
+                                    <label class="col-md-4 control-label">网站描述</label>
+                                    <div class="col-md-4">
+                                        <input class="form-control" type="text" name="site_description" id="site_description"
+                                               value="{{ old('site_description', $site_description) }}"
+                                               placeholder="请输入网站描述">
+                                    </div>
+                                </div>
+
                                 <div class="form-group form-group-sm">
                                     <label class="col-md-4 control-label"></label>
                                     <div class="col-md-4">
