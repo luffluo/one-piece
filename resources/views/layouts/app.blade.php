a9db7f4b06b8fb9ffbd6b3db29cf1a9ad4d6a5fb
luffluo <luffluo@outlook.com>
2018-01-21 22:11:24 +0800

    fix: 修复前台网页在手机端时，输入框获得焦点后会放大的问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,8 @@ ______                            _              _                              
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 
     <title>@yield('title', option('title', config('app.name'))) - {{ option('title', config('app.name')) }}</title>
 
