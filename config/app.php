986d0e282029f0df10435f76e2eb853ded05ab4b
luffluo <luffluo@outlook.com>
2018-12-02 11:42:32 +0800

    feat: Add laravel/telescope:1.0

--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\OptionServiceProvider::class,
         App\Providers\InstallerServiceProvider::class,
