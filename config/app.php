ee39d408625517fd7c54e18db09cdc6a92aee28b
luffluo <luffluo@outlook.com>
2019-05-09 11:19:05 +0800

    remove: laravel/telescope

--- config/app.php
+++ config/app.php
@@ -156,7 +156,6 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\InstallerServiceProvider::class,
     ],
